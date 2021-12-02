b6ce916e97d4468fa8f42c34800a15792d062675
Johnny Dongelmans <johnnydongelmans@gmail.com>
2021-12-02 11:21:56 +0100

    Improve readability of day 1

--- day01.rb
+++ day01.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/ruby
 
 input = File.readlines('inputs/day01.txt').map(&:to_i)
-result1 = input.each_cons(2).count { |c| c[1] > c[0] }
-result2 = input.each_cons(3).map(&:sum).each_cons(2).count { |c| c[1] > c[0] }
+result1 = input.each_cons(2).count { |a, b| b > a }
+result2 = input.each_cons(3).map(&:sum).each_cons(2).count { |a, b| b > a }
 
 puts "Day01::Part1 answer: #{result1}"
 puts "Day01::Part2 answer: #{result2}"
